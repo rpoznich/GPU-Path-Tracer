2c60855ceaf6375e802c7975252a727fbf7b7455
Dung Vu <davidvu98@gmail.com>
2019-05-07 20:59:06 -0500

    working

--- src/main.cc
+++ src/main.cc
@@ -23,7 +23,7 @@
 #include <math.h>
 
 
-#define NUM_BOXES  21
+#define NUM_BOXES  100
 #define PHOTON_MAP_PRECISION 100
 int window_width = 1280, window_height = 720;
 int view_width = 1280, view_height = 720;
@@ -64,6 +64,10 @@ const char* quad_fs =
 
 const char* raytrace_cs =
 #include "shaders/raytrace.comp"
+;
+
+const char* photon_merge_cs =
+#include "shaders/photonmerge.comp"
 ;
 
 
@@ -205,7 +209,7 @@ GLuint createPhotonMapTexture() {
 		glGenTextures(1, &photonMapTexture);
   
         glBindTexture(GL_TEXTURE_CUBE_MAP_ARRAY, photonMapTexture);
-        glTexStorage3D(GL_TEXTURE_CUBE_MAP_ARRAY, 1, GL_RGBA32F, PHOTON_MAP_PRECISION, PHOTON_MAP_PRECISION, 6 * NUM_BOXES * 2);
+        glTexStorage3D(GL_TEXTURE_CUBE_MAP_ARRAY, 1, GL_RGBA16F, PHOTON_MAP_PRECISION, PHOTON_MAP_PRECISION, 6 * NUM_BOXES * 2);
         glBindTexture(GL_TEXTURE_CUBE_MAP_ARRAY, 0);
 
         GLuint texBuffer;
@@ -213,7 +217,7 @@ GLuint createPhotonMapTexture() {
         glBindBuffer(GL_PIXEL_UNPACK_BUFFER, texBuffer);
         int size = 2 * 2 * PHOTON_MAP_PRECISION * PHOTON_MAP_PRECISION * 6 * NUM_BOXES * 2;
         glBufferData(GL_PIXEL_UNPACK_BUFFER, size, NULL, GL_STATIC_DRAW);
-        glClearBufferSubData(GL_PIXEL_UNPACK_BUFFER, GL_RGBA32F, 0, size, GL_RGBA, GL_HALF_FLOAT, NULL);
+        glClearBufferSubData(GL_PIXEL_UNPACK_BUFFER, GL_RGBA16F, 0, size, GL_RGBA, GL_HALF_FLOAT, NULL);
         glBindTexture(GL_TEXTURE_CUBE_MAP_ARRAY, photonMapTexture);
         glTexSubImage3D(GL_TEXTURE_CUBE_MAP_ARRAY, 0, 0, 0, 0, PHOTON_MAP_PRECISION, PHOTON_MAP_PRECISION, 6 * NUM_BOXES * 2,
                 GL_RG, GL_HALF_FLOAT, 0L);
@@ -269,7 +273,7 @@ int main(int argc, char* argv[])
 
 	//terra.createWaterPlane(water_vertices, water_faces, gui);
 
-	glm::vec4 light_position = glm::vec4(38.0, 5, 45.0, 1.0f);
+	glm::vec4 light_position = glm::vec4(39, 14, 39, 1.0f);
 	float near_plane = 1.0f, far_plane = 200.0f;
 	glm::mat4 lightProjection = glm::ortho(-200.0f, 200.0f, -200.0f, 200.0f, near_plane, far_plane);
 	glm::mat4 lightView = glm::lookAt(glm::vec3(light_position), 
@@ -410,7 +414,7 @@ int main(int argc, char* argv[])
 			);
 
 	raytracer_pass.getUniformLocation("cubeMaps");
-	GLuint raytracerTexture = createNew2DTexture(GL_RGBA32F, GL_RGBA, view_width, view_height);
+	GLuint raytracerTexture = createNew2DTexture(GL_RGBA16F, GL_RGBA, view_width, view_height);
 	GLint frameBufferBinding = raytracer_pass.getUniform(raytracer_pass.getUniformLocation("framebuffer"));
 	GLint photonMapsBinding_rt = raytracer_pass.getUniform(raytracer_pass.getUniformLocation("photonMaps"));
 
@@ -446,6 +450,17 @@ int main(int argc, char* argv[])
 			{  }
 			);
 	GLint photonMapsBinding_scatter = photon_scatter_pass.getUniform(photon_scatter_pass.getUniformLocation("photonMaps"));
+
+
+	// //photon merge render pass
+	// RenderDataInput photon_merge_pass_input;
+	// RenderPass photon_merge_pass(-1,
+	// 		photon_merge_pass_input,
+	// 		{ NULL, NULL, NULL, photon_merge_cs},
+	// 		{ },
+	// 		{  }
+	// 		);
+	// GLint photonMapsBinding_merge = photon_merge_pass.getUniform(photon_merge_pass.getUniformLocation("photonMaps"));
 	//Photonmap for the cubes
 	GLuint photonMapTexture = createPhotonMapTexture();
 
@@ -505,7 +520,8 @@ int main(int argc, char* argv[])
  //        glBindImageTexture(photonMapsBinding, 0, 0, true, 0, GL_READ_WRITE, GL_RG16F);
  //        --steps;
 	// }
-
+    std::vector<GLubyte> emptyData(view_width * view_width * 4, 0);
+    int i = 0;
 	while (!glfwWindowShouldClose(window)) {
 		//cube_pass.updateVBO(0,cube_vertices.data(), cube_vertices.size());
 		
@@ -519,47 +535,65 @@ int main(int argc, char* argv[])
 		glDepthFunc(GL_LESS);
 		glCullFace(GL_BACK);
 		
-	
+		// if(gui.isDirty())
+		// {
+		// 	glBindTexture(GL_TEXTURE_2D, raytracerTexture);
+		// 	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, view_width, view_height, GL_BGRA, GL_UNSIGNED_BYTE, &emptyData[0]);
+		// 	glBindTexture(GL_TEXTURE_2D, 0);
+		// }
+		
 		gui.updateMatrices(false);
 		mats = gui.getMatrixPointers();
-		
+
+
 		std::cout<< gui.getCamera() << std::endl;
 		
 
-		photon_pass.setup();
-		CHECK_GL_ERROR(glBindImageTexture(photonMapsBinding, photonMapTexture, 0, true, 0, GL_READ_WRITE, GL_RGBA32F));
-        CHECK_GL_ERROR(glDispatchCompute(worksizeX / workGroupSize[0], worksizeY / workGroupSize[1], 1));
-        glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-        glBindImageTexture(photonMapsBinding, 0, 0, true, 0, GL_READ_WRITE, GL_RGBA32F);
+		
+		if(i < 1000)
+		{
+			photon_pass.setup();
+			CHECK_GL_ERROR(glBindImageTexture(photonMapsBinding, photonMapTexture, 0, true, 0, GL_READ_WRITE, GL_RGBA16F));
+	        CHECK_GL_ERROR(glDispatchCompute(worksizeX / workGroupSize[0], worksizeY / workGroupSize[1], 1));
+	        glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+	        glBindImageTexture(photonMapsBinding, 0, 0, true, 0, GL_READ_WRITE, GL_RGBA16F);
+	    }
 
 
 
-        photon_scatter_pass.setup();
-		CHECK_GL_ERROR(glBindImageTexture(photonMapsBinding_scatter, photonMapTexture, 0, true, 0, GL_READ_WRITE, GL_RGBA32F));
+        if(i > 100)
+        {
+	        photon_scatter_pass.setup();
+			CHECK_GL_ERROR(glBindImageTexture(photonMapsBinding_scatter, photonMapTexture, 0, true, 0, GL_READ_WRITE, GL_RGBA16F));
 
-        CHECK_GL_ERROR(glDispatchCompute(worksizeX / workGroupSize[0], worksizeY / workGroupSize[1], 1));
-        glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-        glBindImageTexture(photonMapsBinding_scatter, 0, 0, true, 0, GL_READ_WRITE, GL_RGBA32F);
+	        CHECK_GL_ERROR(glDispatchCompute(worksizeX / workGroupSize[0], worksizeY / workGroupSize[1], 1));
+	        glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+	        glBindImageTexture(photonMapsBinding_scatter, 0, 0, true, 0, GL_READ_WRITE, GL_RGBA16F);
+	    }
 
 
+        ++i;
         //first pass, render raytracing texture
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        CHECK_GL_ERROR(glBindImageTexture(photonMapsBinding_rt, photonMapTexture, 0, true, 0, GL_READ_WRITE, GL_RGBA32F));
+        		CHECK_GL_ERROR(glBindImageTexture(photonMapsBinding_rt, photonMapTexture, 0, true, 0, GL_READ_WRITE, GL_RGBA16F));
 
-        glBindImageTexture(frameBufferBinding, raytracerTexture, 0, false, 0, GL_WRITE_ONLY, GL_RGBA32F);
+        glBindImageTexture(frameBufferBinding, raytracerTexture, 0, false, 0, GL_WRITE_ONLY, GL_RGBA16F);
         glBindTexture(GL_TEXTURE_CUBE_MAP_ARRAY, photonMapTexture);
         glBindSampler(0, cubeSampler);
 		raytracer_pass.setup();
 		CHECK_GL_ERROR(glDispatchCompute(nextPowerOfTwo(view_width) / workGroupSize[0], nextPowerOfTwo(view_height) / workGroupSize[1], 1));
 		glBindSampler(0, 0);
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
         glBindTexture(GL_TEXTURE_CUBE_MAP_ARRAY, 0);
-        glBindImageTexture(0, 0, 0, false, 0, GL_READ_WRITE, GL_RGBA32F);
+        glBindImageTexture(0, 0, 0, false, 0, GL_READ_WRITE, GL_RGBA16F);
         
         screen_pass.setup();
 		glBindTexture(GL_TEXTURE_2D,raytracerTexture);
 		CHECK_GL_ERROR(glDrawElements(GL_TRIANGLES,
                               quad_indices.size() * 3,
                               GL_UNSIGNED_INT, 0));
+		glBindTexture(GL_TEXTURE_2D,0);
 
 // 		if(terra.updateCube(gui.getCamera()[0], gui.getCamera()[2]) || need_update)
 // 		{
